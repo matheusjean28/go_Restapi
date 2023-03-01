3d87f5b666a41b5ec8a57dc75b01d2ecd69d6340
Dev_matth <45796720+matheusjean28@users.noreply.github.com>
2023-03-01 17:28:48 -0300

    add entities
    this is just for testing

---
+++ domain/entities.go
@@ -0,0 +1,7 @@
+package domain
+
+type Person struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	AGE  int    `json:"age"`
+}
--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/matheusjean28/go_Restapi/domain"
+)
 
 func main() {
 	http.HandleFunc("/person/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
-			w.Write([]s)
-			println("route post")
-		} else if r.Method == "GET" {
-			println("method Get")
+
+			var person domain.Person
+			err := json.NewDecoder(r.Body).Decode(&person)
+			if err != nil {
+				fmt.Printf("error trying to decode r.body. body must be in a json format %s", err.Error())
+				http.Error(w, "error on create a new persn", http.StatusBadRequest)
+				return
+			}
+			if person.ID <= 0 {
+				http.Error(w, "Id must be more than zero ", http.StatusBadRequest)
+				return
+			}
+
+			w.WriteHeader(http.StatusCreated)
+			return
 		}
 		http.Error(w, "not implemented ", http.StatusInternalServerError)
 	})
